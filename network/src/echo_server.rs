bcb3593715be6fc1791de2a4a8955380da4bd0b7
playtime-1967 <samannamnik@proton.me>
2025-02-14 13:52:01 +0000

    refactored echo-server

--- network/src/echo_server.rs
+++ network/src/echo_server.rs
@@ -7,20 +7,19 @@ use hyper::body::Frame;
 use hyper::service::service_fn;
 use hyper::{Method, StatusCode};
 use hyper::{Request, Response};
+use hyper_util::rt::TokioExecutor;
 use hyper_util::rt::TokioIo;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
-use hyper_util::rt::TokioExecutor;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 5948));
     let listener = TcpListener::bind(addr).await?;
     println!("Listening to {}", addr);
 
     loop {
-        //Waits for a new incoming TCP connection on the listener socket
-        //The server processes new connections sequentially and avoids race conditions or idle tasks.
+        //process new connections sequentially and avoids race conditions or idle tasks.
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream); //adapter
 
@@ -38,17 +37,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 async fn echo_service(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    // Super Simple Routing Table
+    //a simple Routing Table
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(full(
-            "Welcome, We have the following apis: \n POST echo, echo/uppercase, echo/reverse",
+            "Welcome, We have the following endpoints: \n POST echo, POST echo/uppercase, POST echo/reverse",
         ))),
         (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
         (&Method::POST, "/echo/uppercase") => {
-            // Map this body's frame to a different type
             let frame_stream = req.into_body().map_frame(|frame| {
                 let frame = if let Ok(data) = frame.into_data() {
-                    // Convert every byte in every Data frame to uppercase
                     data.iter()
                         .map(|byte| byte.to_ascii_uppercase())
                         .collect::<Bytes>()
@@ -62,23 +59,23 @@ async fn echo_service(
             Ok(Response::new(frame_stream.boxed()))
         }
         (&Method::POST, "/echo/reverse") => {
-            // Protect our server from massive bodies.
+            //protect the server from massive bodies.
             let upper = req.body().size_hint().upper().unwrap_or(u64::MAX);
             if upper > 1024 * 64 {
-                let mut resp = Response::new(full("Body too big"));
+                let mut resp = Response::new(full("Body is too big!"));
                 *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
                 return Ok(resp);
             }
 
-            // Await the whole body to be collected into a single `Bytes`...
+            //await the whole body to be collected
             let whole_body = req.collect().await?.to_bytes();
 
-            // Iterate the whole body in reverse order and collect into a new Vec.
+            //reverse the body.
             let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
 
             Ok(Response::new(full(reversed_body)))
         }
-        // Return 404 Not Found for other routes.
+        //return 404 for other routes.
         _ => {
             let mut not_found = Response::new(empty());
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -87,9 +84,9 @@ async fn echo_service(
     }
 }
 
-//BoxBody<Bytes, hyper::Error> is a type alias in hyper for a boxed, dynamically-dispatched implementation of the Body trait
+//BoxBody<D,E>: a boxed, dynamically-dispatched implementation of the Body trait in Hyper.
 fn empty() -> BoxBody<Bytes, hyper::Error> {
-    //The error type of Empty is std::convert::Infallible, which is a type that can never actually exist. This means Empty will never produce an error when used.
+    //Empty: a body that is always empty, means Empty will never produce an error when used.
     Empty::<Bytes>::new()
         .map_err(|never| match never {})
         .boxed()
@@ -99,6 +96,3 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
         .map_err(|never| match never {})
         .boxed()
 }
-
-//How to run
-// curl --location --request GET 'http://127.0.0.1:3000/' \ --header 'Content-Type: application/json' \
